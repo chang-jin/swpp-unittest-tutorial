17132e588ed0dc1ec3f226383ee618ce183f68a1
Woojin Cha <ckdnwls1215@snu.ac.kr>
2021-10-01 11:31:48 +0900

    Update TodoCalendar.js

--- src/containers/TodoCalendar/TodoCalendar.js
+++ src/containers/TodoCalendar/TodoCalendar.js
@@ -38,9 +38,9 @@ class TodoCalendar extends Component {
       <div>
         <div className="link"><NavLink to='/todos' exact>See TodoList</NavLink></div>
         <div className="header">
-          <button onClick={this.handleClickPrev}> prev month </button>
+          <button className="btn_pre" onClick={this.handleClickPrev}> prev month </button>
           {this.state.year}.{this.state.month}
-          <button onClick={this.handleClickNext}> next month </button>
+          <button className="btn_next" onClick={this.handleClickNext}> next month </button>
         </div>
         <Calendar
           year={this.state.year}
