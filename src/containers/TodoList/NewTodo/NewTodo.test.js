d35e08c960b4c55715fd01a31c2188b3b9048aab
Juyoung <godante27@gmail.com>
2021-10-01 11:09:49 +0900

    Implement NewTodo test

--- src/containers/TodoList/NewTodo/NewTodo.test.js
+++ src/containers/TodoList/NewTodo/NewTodo.test.js
@@ -11,9 +11,9 @@ import * as actionCreators from '../../../store/actions/todo';
 
 const stubInitialState = {
   todos: [
-    {id: 1, title: 'TODO_TEST_TITLE_1', done: false},
-    {id: 2, title: 'TODO_TEST_TITLE_2', done: false},
-    {id: 3, title: 'TODO_TEST_TITLE_3', done: false},
+    { id: 1, title: 'TODO_TEST_TITLE_1', done: false },
+    { id: 2, title: 'TODO_TEST_TITLE_2', done: false },
+    { id: 3, title: 'TODO_TEST_TITLE_3', done: false },
   ],
   selectedTodo: null,
 };
@@ -27,13 +27,13 @@ describe('<NewTodo />', () => {
     newTodo = (
       <Provider store={mockStore}>
         <ConnectedRouter history={history}>
-        <Switch>
-          <Route path='/' exact component={NewTodo} />
-        </Switch>
+          <Switch>
+            <Route path='/' exact component={NewTodo} />
+          </Switch>
         </ConnectedRouter>
       </Provider>
     );
-  })
+  });
 
   it('should render NewTodo', () => {
     const component = mount(newTodo);
@@ -42,33 +42,61 @@ describe('<NewTodo />', () => {
   });
 
   it(`should call 'postTodo'`, () => {
-    const spyPostTodo = jest.spyOn(actionCreators, 'postTodo')
-      .mockImplementation(td => { return dispatch => {}; });
+    const spyPostTodo = jest
+      .spyOn(actionCreators, 'postTodo')
+      .mockImplementation((td) => {
+        return (dispatch) => {};
+      });
     const component = mount(newTodo);
     const wrapper = component.find('button');
     wrapper.simulate('click');
     expect(spyPostTodo).toHaveBeenCalledTimes(1);
   });
-  
-  it(`should set state properly on title input`, () => {
-    const title = 'TEST_TITLE'
+
+  it('should set state properly on title input', () => {
+    const title = 'TEST_TITLE';
     const component = mount(newTodo);
-    const wrapper = component.find('input');
+    const wrapper = component.find('input.title');
     wrapper.simulate('change', { target: { value: title } });
     const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
     expect(newTodoInstance.state.title).toEqual(title);
     expect(newTodoInstance.state.content).toEqual('');
   });
 
-  it(`should set state properly on content input`, () => {
-    const content = 'TEST_CONTENT'
+  it('should set state properly on content input', () => {
+    const content = 'TEST_CONTENT';
     const component = mount(newTodo);
-    const wrapper = component.find('textarea');
+    const wrapper = component.find('textarea.content');
     wrapper.simulate('change', { target: { value: content } });
     const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
     expect(newTodoInstance.state.title).toEqual('');
     expect(newTodoInstance.state.content).toEqual(content);
   });
+
+  it('should set state properly on year input', () => {
+    const year = 2021;
+    const component = mount(newTodo);
+    const wrapper = component.find('input.year');
+    wrapper.simulate('change', { target: { value: year } });
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.dueDate.year).toEqual(2021);
+  });
+
+  it('should set state properly on month input', () => {
+    const month = 9;
+    const component = mount(newTodo);
+    const wrapper = component.find('input.month');
+    wrapper.simulate('change', { target: { value: month } });
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.dueDate.month).toEqual(9);
+  });
+
+  it('should set state properly on date input', () => {
+    const date = 1;
+    const component = mount(newTodo);
+    const wrapper = component.find('input.date');
+    wrapper.simulate('change', { target: { value: date } });
+    const newTodoInstance = component.find(NewTodo.WrappedComponent).instance();
+    expect(newTodoInstance.state.dueDate.date).toEqual(1);
+  });
 });
-
-
