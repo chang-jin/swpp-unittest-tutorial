c59cb4f449a48004011790f57fd70e3d4baf9b39
KIMJunOh <kjo1797@gmail.com>
2021-10-02 03:49:07 +0900

    Calendar 71.43%

---
+++ src/components/Calendar/Calendar.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Calendar from './Calendar';
+import { Table } from 'semantic-ui-react';
+
+describe('<Calendar />', () => {
+    it('should render Calender without error', () => {
+        const component = shallow(<Calendar />);
+        const wrapper = component.find(Table.Body);
+
+        expect(wrapper.length).toBe(1);
+    })
+
+    it('should render rows without error', () => {
+        const component = shallow(<Calendar year={2021} month={10} todos={[]}/>);
+        const wrapper = component.find(Table.Row);
+
+        expect(wrapper.length).toBe(6);
+    })
+
+    it('should render cells without error', () => {
+        const component = shallow(<Calendar year={2021} month={10} todos={[]} />);
+        const wrapper = component.find(Table.Cell);
+
+        expect(wrapper.length).toBe(35);
+    })
+
+    it('should render todo without error', () => {
+        const stubTodos = [
+            { id: 1, year: 2021, month: 10, date: 1, done: false, title: "TEST_TODO"},
+        ];
+        const component = shallow(<Calendar year={2021} month={10} todos={stubTodos} />);
+        const wrapper = component.find('.todoTitle');
+
+        //expect(wrapper.length).toBe(1);
+        console.log(component.debug());
+    })
+})
