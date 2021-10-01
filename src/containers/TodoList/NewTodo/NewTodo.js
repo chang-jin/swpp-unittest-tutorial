8d0a792f3611cd6629d0df92d812f76293534377
eunbin1227 <eunbin1227@snu.ac.kr>
2021-10-01 16:16:44 +0900

    NewTodo complete

--- src/containers/TodoList/NewTodo/NewTodo.js
+++ src/containers/TodoList/NewTodo/NewTodo.js
@@ -39,31 +39,35 @@ class NewTodo extends Component {
         <h1>Add a New Todo!</h1>
         <label>Title</label>
         <input
+          className="title"
           type="text"
           value={this.state.title}
           onChange={(event) => this.setState({ title: event.target.value })}
         ></input>
         <label>Content</label>
-        <textarea rows="4" type="text" value={this.state.content}
+        <textarea className="content" rows="4" type="text" value={this.state.content}
           onChange={(event) => this.setState({ content: event.target.value })}
         >
         </textarea>
         <label>Due Date</label>
-        year <input
+        year{""} <input
+          className="year"
           type="text"
           value={this.state.dueDate.year}
           onChange={(event) => this.setState({
             dueDate: {...this.state.dueDate, year: event.target.value }
           })}
         ></input>
-        month <input
+        month{""} <input
+          className="month"
           type="text"
           value={this.state.dueDate.month}
           onChange={(event) => this.setState({
             dueDate: {...this.state.dueDate, month: event.target.value }
           })}
         ></input>
-        date <input
+        date{""} <input
+          className="date"
           type="text"
           value={this.state.dueDate.date}
           onChange={(event) => this.setState({
