1a50f22cbdb3bb974b227a92c4995e5986fd4e65
KIMJunOh <kjo1797@gmail.com>
2021-10-02 16:03:48 +0900

    all green!

--- src/components/Calendar/Calendar.test.js
+++ src/components/Calendar/Calendar.test.js
@@ -4,8 +4,8 @@ import Calendar from './Calendar';
 import { Table } from 'semantic-ui-react';
 
 const stubTodos = [
-    { id: 1, year: 2021, month: 9, date: 1, done: true, title: "TEST_TODO_1"},
-    { id: 2, year: 2021, month: 9, date: 2, done: false, title: "TEST_TODO_2"},
+    { id: 1, title: "TEST_TODO_1", done: true, year: 2021, month: 9, date: 1},
+    { id: 2, title: "TEST_TODO_2", done: false, year: 2021, month: 9, date: 2},
 ];
 
 describe('<Calendar />', () => {
@@ -14,28 +14,44 @@ describe('<Calendar />', () => {
         const wrapper = component.find(Table.Body);
 
         expect(wrapper.length).toBe(1);
-    })
+    });
 
     it('should render rows without error', () => {
         const component = shallow(<Calendar year={2021} month={9} todos={stubTodos} />);
         const wrapper = component.find(Table.Row);
 
         expect(wrapper.length).toBe(6);
-    })
+    });
 
     it('should render cells without error', () => {
         const component = shallow(<Calendar year={2021} month={9} todos={stubTodos} />);
         const wrapper = component.find(Table.Cell);
 
         expect(wrapper.length).toBe(35);
-    })
+    });
 
-    // I think this should render todo in Table Cell, but It does not work at all.
     it('should render todo without error', () => {
         const component = shallow(<Calendar year={2021} month={9} todos={stubTodos} />);
         const wrapper = component.find('.todoTitle');
 
-        expect(wrapper.length).toBe(0);
-        //console.log(component.debug());
+        expect(wrapper.length).toBe(2);
+    });
+
+    it('should clicking todo dispatch clickDone without error when done=true', () => {
+        const stubClickDone = jest.fn((id) => {})
+        const component = shallow(<Calendar year={2021} month={9} todos={stubTodos} clickDone={stubClickDone}/>);
+        const wrapper = component.find('.todoTitle').at(0);
+        wrapper.simulate('click');
+
+        expect(stubClickDone).toHaveBeenCalledTimes(1);
+    })
+
+    it('should clicking todo dispatch clickDone without error when done=false', () => {
+        const stubClickDone = jest.fn((id) => {})
+        const component = shallow(<Calendar year={2021} month={9} todos={stubTodos} clickDone={stubClickDone}/>);
+        const wrapper = component.find('.todoTitle').at(1);
+        wrapper.simulate('click');
+
+        expect(stubClickDone).toHaveBeenCalledTimes(1);
     })
 })
